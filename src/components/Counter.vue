ef83b18eb859fdd12804b98396926ad4127c0452
USER <burlacurodionmd@gmail.com>
2022-11-02 17:52:48 +0200

    add project

---
+++ src/components/Counter.vue
@@ -0,0 +1,26 @@
+<template> 
+    <button type="button" @click="increment">count is {{ count }}</button>
+</template>
+<script lang="ts">
+export default {
+    props: {
+        initValue: {
+            type: Number,
+            default: 0
+        }
+    },
+    data() {
+        return {count: -1 }
+    },
+    beforeMount() {
+        this.count = this.initValue
+    },
+    emits: ['update:value'],
+    methods: {
+        increment() {
+            this.count += 2
+            this.$emit('update:value', this.count)
+        }
+    }
+}
+</script>
